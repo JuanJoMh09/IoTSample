57f10a095c8398ed803c5fd98af0038d83fe57b3
Juan Jose <juanjomh09@gmail.com>
2018-07-26 11:26:53 -0600

    Clase 20

--- platziverse-db/tests/agent-tests.js
+++ platziverse-db/tests/agent-tests.js
@@ -3,6 +3,9 @@
 const test = require('ava') //libreria para pruebas unitarias
 const proxyquire = require('proxyquire')
 const sinon = require('sinon')
+
+const agentFixtures = require('./fixtures/agent')//hace el llamado al datos falsos o quemados
+
 let config = {
    logging: function() {}
 }
@@ -11,9 +14,12 @@ let MetricStub = { //los stub simulan modelos, les pasan datos quemados para ver
     belongsTo: sinon.spy() //sinon es una funcion que da diferentes datos como por ejemplo ver cuantas veces se requirio el metodo
 }
 
+let id = 1
+let single = Object.assign({}, agentFixtures.single) //clonamos el single para no estar probando solo con la instancia de fixtures, si no tener otra instancia por aparte 
 let AgentStub = null
 let sandbox = null
 let db = null
+
 
 test.beforeEach(async () => {
     sandbox = sinon.sandbox.create() //Cuando una funcion como agent es requerida muchas veces en varias partes del codigo los datos de cuantas veces se uso se acumulan y no funciona, se crea un sandbox para que esa estadistica se reinicie cada vez que se use
@@ -40,4 +46,10 @@ test.serial('Setup', t => {
     t.true(AgentStub.hasMany.calledWith(MetricStub), 'Argument should be the MetricModel')     
     t.true(MetricStub.belongsTo.called, 'MetricModel.belongsTo was executed')
     t.true(MetricStub.belongsTo.calledWith(AgentStub), 'Argument should be the AgentModel')
+})
+
+test.serial('Agent#findById', async t =>{
+    let agent = await db.Agent.findById(id)
+
+    t.deepEqual(agent, agentFixtures.byId(id), 'should be the same')
 })
